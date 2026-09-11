2e8cbe2804f996e0ddd88a9d7c933c0eac788b97
Pavlo Matviienko <pavlo.matviienko@flix.com>
2026-09-11 12:37:54 +0200

    fix: show SMART freshness and refresh disk readings

--- src/components/Storage/DriveItem.vue
+++ src/components/Storage/DriveItem.vue
@@ -21,13 +21,18 @@
 			</div>
 			<div class="is-flex is-align-items-center status pri-min-width">
 				<div>
-					<p class="has-text-left is-size-7 mb-3">{{ $t('Health') }}: <b v-if="item.health"
+					<p class="has-text-left is-size-7 mb-3">{{ $t('Health') }}: <b v-if="item.health === 'true' && item.smart_state !== 'unavailable'"
 																				   class="has-text-success">{{
 						$t('Healthy') }}</b><b
-					v-if="!item.health" class="has-text-danger">{{ $t('Damage') }}</b></p>
+					v-else-if="item.health === 'false' && item.smart_state !== 'unavailable' && (item.smart_state !== 'sleeping' || item.smart_sampled_at)" class="has-text-danger">{{ $t('Damage') }}</b><b v-else>N/A</b></p>
+					<p v-if="item.smart_state === 'sleeping'" class="has-text-left is-size-7">{{ $t('Disk sleeping') }}</p>
+					<p v-if="item.smart_state === 'unavailable'" class="has-text-left is-size-7">{{ $t('SMART data unavailable') }}</p>
 					<p class="has-text-left is-size-7 ">{{ $t('Temp') }}: <b v-if="item.temperature > 0">{{
 						item.temperature }}°C
 						/ {{ item.temperature | toFahrenheit }}°F</b> <b v-else>N/A</b></p>
+					<p v-if="item.smart_sampled_at" class="has-text-left is-size-7 has-text-grey">
+						{{ $t('SMART last measured') }}: {{ new Date(item.smart_sampled_at * 1000).toLocaleString($i18n.locale.replace('_', '-')) }}
+					</p>
 				</div>
 			</div>
 		</div>
